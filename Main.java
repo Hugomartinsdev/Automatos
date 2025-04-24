a89b49cccca92fc8fef38ee83d1795f0f42fb802
Hugo Martins <130413000+Hugomartinsdev@users.noreply.github.com>
2025-04-24 18:51:25 -0300

    Mudança e correção do codigo

--- Main.java
+++ Main.java
@@ -1,49 +1,21 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main{
 	public static void main(String[] args){
    	 
     	Scanner scan = new Scanner(System.in);
-    	System.out.println("CONVERSOR DE BINÁRIO PARA DECIMAL");
-    	System.out.println("Digite os dígitos do binário: ");
-    	System.out.println("(quando terminar o binário, digite X)");
+    	System.out.println("digite a string do binário: ");
+    	String input = scan.nextLine();
    	 
-    	int bitsAmount = 0;
-    	ArrayList<Integer> bitsArray = new ArrayList<>();
-    	String input = "";
+       	Pattern pattern = Pattern.compile("[0-1]+");
+    	Matcher matcher = pattern.matcher(input);
    	 
-    	do{
-        	System.out.println("Dígito " + (bitsAmount+1) + ": ");
-        	input = scan.nextLine();
-        	switch(input){
-            	case "0":
-                	bitsArray.add(0);
-                	bitsAmount++;
-            	break;
-            	case "1":
-                	bitsArray.add(1);
-                	bitsAmount++;
-            	break;
-            	case "X":
-                	System.out.println("Calculando número: ");
-            	break;
-            	default:
-                	System.out.println("ERRO: somente 0s, 1s e X são permitidos! ");
-            	break;
-        	}
-    	}while(!input.equals("X"));
-   	 
-    	int decimalValue = 0;
-   	 
-    	if(bitsAmount != 0){
-        	for(int i = 0; i < bitsAmount; i++){
-            	decimalValue += Math.pow(2, i) * bitsArray.get(i);
-        	}
-    	}
-   	 
-    	System.out.println("Valor em decimal: " + decimalValue);
+    	if (matcher.matches()) {
+            System.out.println("é binario");
+        } else {
+            System.out.println("nao é binario");
+		}
 	}
 }
-
-
